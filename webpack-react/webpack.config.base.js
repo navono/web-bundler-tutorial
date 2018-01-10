c952f0bf6ff8ec3c2c6832dd9b4e6dd8aa555a52
navono <navono007@gmail.com>
2018-01-10 12:51:05 +0800

    add webpack-merge to extract common config

---
+++ webpack-react/webpack.config.base.js
@@ -0,0 +1,94 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractPlugin = new ExtractTextPlugin({
+  filename: './assets/css/app.css'
+});
+
+const baseConfig = {
+  // absolute path for project root
+  context: path.resolve(__dirname, 'src'),
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './assets/js/[name].bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        include: /src/,
+        exclude: /node_modules/,
+        use:
+        [
+          {
+            loader: "babel-loader",
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
+        use: extractPlugin.extract({
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true
+              }
+            }
+          ],
+          fallback: 'style-loader'
+        })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      // file-loader(for images)
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: 
+            {
+              name: '[name].[ext]',
+              outputPath: './assets/media/' 
+            } 
+          } 
+        ] 
+      },
+      // file-loader(for fonts)
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React',
+      template: 'index.html',
+    }),
+  ]
+}
+
+module.exports = {
+  baseConfig,
+  extractPlugin
+}
--- webpack-react/webpack.config.dev.js
+++ webpack-react/webpack.config.dev.js
@@ -1,122 +1,47 @@
 const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const merge = require('webpack-merge');
+const { baseConfig, extractPlugin } = require('./webpack.config.base');
 
-const extractPlugin = new ExtractTextPlugin({
-  filename: './assets/css/app.css'
-});
 
-module.exports = {
-  // absolute path for project root
-  context: path.resolve(__dirname, 'src'),
-
-  // Dev only
-  devtool: 'inline-source-map',
-  devServer: {
-    // static files served from here
-    contentBase: path.resolve(__dirname, "./dist"),
-    compress: true,
-    hot: true,
-    port: 3000,
-    historyApiFallback: true,
-   //  stats: 'errors-only',
-    open: true,
+const devConfig = merge(
+  baseConfig,
+  {
+     // Dev only
+    devtool: 'inline-source-map',
+    devServer: {
+      // static files served from here
+      contentBase: path.resolve(__dirname, "./dist"),
+      compress: true,
+      hot: true,
+      port: 3000,
+      historyApiFallback: true,
+      //  stats: 'errors-only',
+      open: true,
    },
 
-  entry: [
-    'react-hot-loader/patch',
-    // dev-server: 遇到错误会重新刷新浏览器
-    // only-dev-server: 遇到错误不会重新刷新浏览器，React App推荐使用。因为不会重置状态
-    // 'webpack/hot/only-dev-server',
+    entry: [
+      'react-hot-loader/patch',
+      // dev-server: 遇到错误会重新刷新浏览器
+      // only-dev-server: 遇到错误不会重新刷新浏览器，React App推荐使用。因为不会重置状态
+      // 'webpack/hot/only-dev-server',
 
-    // relative path declaration
-    './index',
-  ],
+      // relative path declaration
+      './index',
+    ],
 
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: './assets/js/[name].bundle.js',
-  },
-
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        include: /src/,
-        exclude: /node_modules/,
-        use:
-        [
-          {
-            loader: "babel-loader",
-          }
-        ]
-      },
-      {
-        test: /\.scss$/,
-        include: [path.resolve(__dirname, 'src', 'assets', 'scss')],
-        use: extractPlugin.extract({
-          use: [
-            {
-              loader: 'css-loader',
-              options: {
-                sourceMap: true
-              }
-            },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true
-              }
-            }
-          ],
-          fallback: 'style-loader'
-        })
-      },
-      {
-        test: /\.html$/,
-        use: ['html-loader']
-      },
-      // file-loader(for images)
-      {
-        test: /\.(jpg|png|gif|svg)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: 
-            {
-              name: '[name].[ext]',
-              outputPath: './assets/media/' 
-            } 
-          } 
-        ] 
-      },
-      // file-loader(for fonts)
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
-      }
+    plugins: [
+      // Dev only
+      new webpack.HotModuleReplacementPlugin(),
+      
+      // bundle size analyzer
+      // new BundleAnalyzerPlugin(),
+  
+      // extract-text-webpack-plugin instance
+      extractPlugin
     ]
-  },
+  }
+)
 
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'React',
-      template: 'index.html',
-    }),
-
-    // Dev only
-    new webpack.HotModuleReplacementPlugin(),
-    
-    // bundle size analyzer
-    // new BundleAnalyzerPlugin(),
-
-    // extract-text-webpack-plugin instance
-    extractPlugin
-  ]
-}
+module.exports = devConfig;
